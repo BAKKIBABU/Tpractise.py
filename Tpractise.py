593f93dcf41a2e5ce0c04efb5c9f41b820d66929
BAKKIBABU <108388125+BAKKIBABU@users.noreply.github.com>
2022-08-09 12:59:12 +0530

    Add files via upload

---
+++ Tpractise.py
@@ -0,0 +1,50 @@
+# print("naveen")
+# print('naveen's laptop')    # SyntaxError: unterminated string literal (detected at line 2)
+# print("naveen's laptop")      # naveen's laptop
+# print('naveen "laptop"')      # naveen "laptop"
+# print('navin's"laptop"')      #SyntaxError: unterminated string literal (detected at line 5)
+# print('navin\'s"laptop"')       # navin's"laptop"
+# print("naveen"+"naveen")        # naveennaveen
+# print(10*"babu")                  # babubabubabubabubabubabubabubabubabubabu
+# print("c:\docs\navven")          # \n it is special meaning in python \n it means new line
+# print(r'c:\docs\navven')           # c:\docs\navven
+
+# """variables"""
+# """first what is variable"""
+# """ so basically variable is contain we can put the values """
+# x=2
+# x+3
+
+# y=3
+# x+y
+# x=9
+# x=100
+# print(x+y)
+# name='youtube'
+# name='youtube'
+# name+"rocks"
+# print(name)      
+# name[0]
+# name[6]
+# print(name)
+# print[1:4]          # TypeError: 'builtin_function_or_method' object is not subscriptable 
+# myname='naveenreddy'
+# len(myname)
+
+# """ how will assaign the multiple values"""
+# nums = [25,12,36,95,14]
+# print(nums)                 # [25, 12, 36, 95, 14] 
+# print(nums[0])
+# print(nums[1])
+# print(nums[2])
+# print(nums[3])
+# print(nums[4])
+# print(nums[2:])               # [36, 95, 14]
+# print(nums[-1])                  # 14 
+# print(nums[-5])                 # 25
+# names=['ravi','babu','goutham']
+# print(names)                     # ['ravi', 'babu', 'goutham']
+# values = [7.5,"babu",25]
+# print(values)
+# mil = [nums,names]
+# print(mil)
